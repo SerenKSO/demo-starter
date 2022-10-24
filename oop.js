b5eb8f84698eb33bef67597857cd27d860a1a88a
seren <serenkso@hotmail.com>
2022-10-24 22:41:16 +0300

    changes

--- oop.js
+++ oop.js
@@ -1,27 +1,29 @@
-//  Class
-export default class Ev{
-    constructor(odaSayisi, kat, cephe, aidat, fiyat){
-        this.odaSayisi = odaSayisi;
-        this.kat = kat;
-        this.cephe = cephe;
-        this.aidat = aidat;
-        this.fiyat = fiyat;
+// OOP - Object Oriented Programming
+// Nesneye yönelimli Programlama
+
+export default class Ev {
+    //var odaSayisi;
+    constructor(oda, kat, cephe, aidat, fiyat) {
+      this.odaSayisi = oda;
+      this.kat = kat;
+      this.cephe = cephe;
+      this.aidat = aidat;
+      this.fiyat = fiyat;
     }
-
-
-tanit(){
-    console.log(this);
-}
-
-tanit(){
-    console.log("tanit2", this);
-}
-}
-
-export const ev1 = new Ev(3,2,"Kuzey", 100, 2000);
-ev1.tanit();
-
-function evOlustur (oda, kat, cephe, aidat, fiyat){
+  
+    tanit() {
+      console.log(this);
+    }
+  
+    tanit() {
+      console.log("tanit2", this);
+    }
+  }
+  
+  export const ev1 = new Ev(3, 2, "Kuzey", 100, 5000);
+  ev1.tanit();
+  
+  function evOlustur(oda, kat, cephe, aidat, fiyat) {
     this.odaSayisi = oda;
     this.kat = kat;
     this.cephe = cephe;
@@ -35,11 +37,4 @@ function evOlustur (oda, kat, cephe, aidat, fiyat){
     console.log("tanit2", this);
   };
   const ev2 = new evOlustur(3, 2, "Kuzey", 100, 5000);
-  ev2.tanit()
-
-class Customer{
-    constructor(firstName, lastName){
-        this.firstName=firstName;
-        this.lastName=lastName;
-    }
-}
+  ev2.tanit();
